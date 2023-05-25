a35f7b7bf012bb8e95b839b1199cbdf245c60df1
wrhsd1 <wangrenhuan@gmail.com>
2023-05-25 10:22:17 +0800

    Update +server.js

--- src/routes/api/generate-idea/+server.js
+++ src/routes/api/generate-idea/+server.js
@@ -1,18 +1,44 @@
-import { IDEA_GENERATOR_EXPLANATION, } from "$lib/constants";
+import { IDEA_GENERATOR_EXPLANATION,  } from "$lib/constants";
 import { error, json } from '@sveltejs/kit';
+import { ChatGPTBrowserClient } from '@waylaidwanderer/chatgpt-api';
 import {
-    IDEA_GENERATOR_PAINTER,
+  IDEA_GENERATOR_PHOTOGRAPHER,
+  IDEA_GENERATOR_PAINTER,
+  // IDEA_GENERATOR_CRAZY_ARTIST,
+  IDEA_GENERATOR_FASHION_DESIGNER,
+  IDEA_GENERATOR_STREET_PHOTOGRAPHER,
+  IDEA_GENERATOR_ARCHITECT,
+  IDEA_GENERATOR_MOVIE_CONCEPT_ARTIST,
+  IDEA_GENERATOR_GRAPHIC_DESIGNER,
+  IDEA_GENERATOR_GRAPHIC_NOVEL_ARTIST,
 } from "$lib/constants.js";
-import { env } from 'process';
 
 const personalities = {
-    painter: IDEA_GENERATOR_PAINTER,
+  photographer: IDEA_GENERATOR_PHOTOGRAPHER,
+  painter: IDEA_GENERATOR_PAINTER,
+  // crazy_artist: IDEA_GENERATOR_CRAZY_ARTIST,
+  fashion_designer: IDEA_GENERATOR_FASHION_DESIGNER,
+  street_photographer: IDEA_GENERATOR_STREET_PHOTOGRAPHER,
+  architect: IDEA_GENERATOR_ARCHITECT,
+  movie_concept_artist: IDEA_GENERATOR_MOVIE_CONCEPT_ARTIST,
+  graphic_designer: IDEA_GENERATOR_GRAPHIC_DESIGNER,
+  graphic_novel_artist: IDEA_GENERATOR_GRAPHIC_NOVEL_ARTIST,
+  weight_master: IDEA_GENERATOR_PHOTOGRAPHER
+
 };
 
 export async function POST({ request, fetch }) {
     const { personality_key, api_key } = await request.json();
     const personality = personalities[personality_key];
+    const accessToken = (api_key === "1") ? `${env.KEY}` : api_key;
+    const clientOptions = {
+    reverseProxyUrl: `${env.ENDPOINT}`,
+    accessToken: accessToken,
 
+};
+const chatGptClient = new ChatGPTBrowserClient(clientOptions);
+
+    
     if (!personality_key || !api_key) {
         throw error(400, "Invalid request. Please provide prompt_text, personality_key, and api_key.");
     }
@@ -21,28 +47,11 @@ export async function POST({ request, fetch }) {
         const max_tokens = 40;
         const stringMessages = `${IDEA_GENERATOR_EXPLANATION}\n${personality}\nGive me a creative idea`;
         console.log(stringMessages);
+        const response = await chatGptClient.sendMessage( stringMessages );
 
-        const data = JSON.stringify({
-            message: stringMessages,
-            max_tokens: max_tokens,
-            n: 1,
-            stop: null,
-            temperature: 1.0,
-        });
-
-        const response = await fetch(`${env.ENDPOINT}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                message: stringMessages
-            })
-        });
-
-        const data1 = await response.json();
+        const data1 = response;
         console.log(data1);
-        if (data.error) {
+        if (data1.error) {
             throw new Error(data.error.message)
         }
         const replies = [data1.response.trim()];
